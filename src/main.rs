6e7d8e807f9633f2f1d38d715a829bbe5d0af454
William Brawner <me@wbrawner.com>
2022-07-24 16:46:33 -0600

    Use port 4762 by default

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4762").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
