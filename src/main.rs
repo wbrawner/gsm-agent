f481dcfbf76913b0d0e3cecf062314b230f73394
William Brawner <me@wbrawner.com>
2022-07-24 22:41:02 -0600

    Improve error handling for incoming TCP connections

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,12 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4762").unwrap();
     let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| {
-            handle_connection(stream);
-        });
+        match stream {
+            Ok(s) => pool.execute(|| {
+                handle_connection(s);
+            }),
+            Err(e) => println!("invalid stream: {:?}\n", e),
+        }
     }
 }
 
